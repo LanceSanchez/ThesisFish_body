5a0cf54d734a9701ba181c0e0686ecaac37a2aa9
LanceSanchez <lancesanchez16@gmail.com>
2019-10-30 03:05:18 +0800

    Latest

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ latest=$(ls -t /content/drive/Thesis_body/snapshot/*.caffemodel | head -n 1)
 if test -z $latest; then
 	exit 1
 fi
-/content/caffe/build/tools/caffe train -solver="ThesisFish_body/solver_test.prototxt" \
+/content/caffe/build/tools/caffe train -solver="solver_test.prototxt" \
 --weights=$latest \
 -gpu 0
